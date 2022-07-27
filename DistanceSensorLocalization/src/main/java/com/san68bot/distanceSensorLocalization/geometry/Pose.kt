f08af7769ecebe2c8d144e692fa01a2209b341cb
sanjay <san68bot@gmail.com>
2022-07-27 16:49:46 -0400

    Major Changes

--- DistanceSensorLocalization/src/main/java/com/san68bot/distanceSensorLocalization/geometry/Pose.kt
+++ DistanceSensorLocalization/src/main/java/com/san68bot/distanceSensorLocalization/geometry/Pose.kt
@@ -1,17 +1,12 @@
 package com.san68bot.distanceSensorLocalization.geometry
 
-import com.acmerobotics.roadrunner.geometry.Pose2d
-
 data class Pose(
     val point: Point,
     val rad: Double
 ) {
     constructor(x: Double, y: Double, rad: Double) : this(Point(x, y), rad)
-    override fun toString(): String {
-        return "x: ${ point.x }, y: ${ point.y }, rad: $rad"
-    }
+    override fun toString(): String = "x: $x, y: $y, rad: $rad"
 
     val x = point.x
     val y = point.y
-    fun toPose2d(): Pose2d = Pose2d(x, y, rad)
 }
